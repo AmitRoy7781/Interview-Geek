f413c77594600fe5acb7a29c8e8482ffdebf010e
AmitRoy7 <aroy7298@gmail.com>
2019-03-01 16:41:44 +0600

    Video Chat Implemented

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
+
+@app.route('/p2pchatting')
+def video_chat():
+    return render_template("videoChat.html")
+
+
+@app.route('/screenSharing')
+def screen_share():
+    return render_template("screenSharing.html")
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000,debug=True)
